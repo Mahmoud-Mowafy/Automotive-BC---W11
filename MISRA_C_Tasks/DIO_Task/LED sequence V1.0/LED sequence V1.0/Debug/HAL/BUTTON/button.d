ec6b325b6ea9b0d969de30fd4371829929719981
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-06-06 18:09:03 +0300

    adding MISRA-C DIO task

---
+++ MISRA_C_Tasks/DIO_Task/LED sequence V1.0/LED sequence V1.0/Debug/HAL/BUTTON/button.d
@@ -0,0 +1,34 @@
+HAL/BUTTON/button.d HAL/BUTTON/button.o: ../HAL/BUTTON/button.c \
+ ../HAL/BUTTON/button.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../HAL/BUTTON/../../MCAL/DIO/dio.h \
+ ../HAL/BUTTON/../../MCAL/DIO/../../Utilities/Register_file.h \
+ ../HAL/BUTTON/../../MCAL/DIO/../../Utilities/types.h \
+ ../HAL/BUTTON/../../MCAL/DIO/../../Utilities/types.h
+
+../HAL/BUTTON/button.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../HAL/BUTTON/../../MCAL/DIO/dio.h:
+
+../HAL/BUTTON/../../MCAL/DIO/../../Utilities/Register_file.h:
+
+../HAL/BUTTON/../../MCAL/DIO/../../Utilities/types.h:
+
+../HAL/BUTTON/../../MCAL/DIO/../../Utilities/types.h:
---
+++ MISRA_C_Tasks/DIO_Task/LED sequence V1.0/LED sequence V1.0/main.c
@@ -0,0 +1,21 @@
+/*
+ * LED sequence V1.0.c
+ *
+ * Created: 4/7/2023 10:55:02 PM
+ * Author : Mahmoud
+ */ 
+
+
+#include "App/app.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    App_init();
+    while (1) 
+    {
+      App_start();
+    }
+}
+
